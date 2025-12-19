0decd2c49922e05c3ccae7cac65e7ee9b0205e86
lunlogan33-lang <lunlogan33@gmail.com>
2025-12-19 03:26:35 +0000

    Update and rename test1 to test1.py

---
+++ test_file/test1.py
@@ -0,0 +1,16 @@
+import numpy as np
+import os
+from src.model import (create_grid, advect)
+def test_case1_run():
+    x = create_grid(20.0, 0.2, 300.0)
+    dt = 10.0
+    theta_init = np.zeros_like(x)
+    theta_init[0] = 250.0  # pulse at left
+
+    U = 0.1
+
+    C_hist, time_grid = advect(theta_init, U, 0.2, dt, T)
+
+    assert C_hist.shape == (len(t), len(x))
+
+    assert C_hist.min() >= -1e-6
