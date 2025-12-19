9901e6e14514b75c97b1a34c7b89e9e1c3c9bd2f
lunlogan33-lang <lunlogan33@gmail.com>
2025-12-19 03:26:20 +0000

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,17 +1,15 @@
 import numpy as np
 import os
-from src.model import (
-    read_boundary_conditions, interpolate_conditions,
-    create_grid, advect)
+from src.model import (create_grid, advect)
 def test_case1_run():
-    x, t = create_grid(20.0, 0.2, 300.0, 10.0)
-    
+    x = create_grid(20.0, 0.2, 300.0)
+    dt = 10.0
     theta_init = np.zeros_like(x)
     theta_init[0] = 250.0  # pulse at left
 
     U = 0.1
 
-    C_hist = advect(theta_init, U, 0.2, 10.0, t)
+    C_hist, time_grid = advect(theta_init, U, 0.2, dt, T)
 
     assert C_hist.shape == (len(t), len(x))
 
